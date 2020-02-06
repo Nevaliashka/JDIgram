003b682dc73f310bbe4f23d46e3f16bf5297da53
Nevaliashka <chasovskoy.oleg@mail.ru>
2020-02-06 16:10:48 +0300

    Remove files

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,6 +1,15 @@
 class PersonsController < ApplicationController
+  #for push in GitHub
+  # git rm -r -f --cached .
+  # git add .
+  # git commit -m "Remove files"
+  # git push -u origin master
 
   def index
     @text = "Hellow World!"
   end
 end
+
+
+
+
